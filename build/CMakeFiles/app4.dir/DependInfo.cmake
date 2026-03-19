a887305518bd1ae155caa03db2ed6eaa62dc1492
Xiao Feng <1725341408lin@gmail.com>
2026-03-19 12:28:11 -0700

    完成app4第一阶段开发

---
+++ build/CMakeFiles/app4.dir/DependInfo.cmake
@@ -0,0 +1,100 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/feng/xiaozhi/work/app4/main.c" "/home/feng/xiaozhi/work/app4/build/CMakeFiles/app4.dir/main.c.o"
+  "/home/feng/xiaozhi/work/app4/page_test.c" "/home/feng/xiaozhi/work/app4/build/CMakeFiles/app4.dir/page_test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "LV_CONF_INCLUDE_SIMPLE"
+  "SIMULATOR_LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  ".././res"
+  ".././res/font"
+  ".././res/image"
+  "../../app_sdk/lvgl"
+  "../../app_sdk/lvgl/demos"
+  "../../app_sdk/platform/x86linux/src/porting"
+  "../../app_sdk/component/font"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/.."
+  "/home/feng/xiaozhi/work/app_sdk/lvgl"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/core"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/draw"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/draw/nxp_pxp"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/draw/nxp_vglite"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/draw/sdl"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/draw/stm32_dma2d"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/draw/sw"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/layouts/flex"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/layouts/grid"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/bmp"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/ffmpeg"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/freetype"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/fsdrv"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/gif"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/png"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/qrcode"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/rlottie"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/libs/sjpg"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/others"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/others/gridnav"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/others/monkey"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/others/snapshot"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/others/fragment"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/others/imgfont"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/others/msg"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/others/ime"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/themes"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/themes/basic"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/themes/default"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/themes/mono"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/animimg"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/calendar"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/chart"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/colorwheel"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/imgbtn"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/keyboard"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/led"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/list"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/menu"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/meter"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/msgbox"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/span"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/spinbox"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/spinner"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/tabview"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/tileview"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/extra/widgets/win"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/font"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/gpu"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/hal"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/misc"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/src/widgets"
+  "/home/feng/xiaozhi/work/app_sdk/lvgl/../platform/x86linux/include"
+  "/home/feng/xiaozhi/work/app_sdk/platform/x86linux/src/porting"
+  "/home/feng/xiaozhi/work/app_sdk/platform/x86linux/src/porting/device"
+  "/home/feng/xiaozhi/work/app_sdk/platform/x86linux/src/../../../component/hal"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/feng/xiaozhi/work/app4/build/lvgl/CMakeFiles/lvgl.dir/DependInfo.cmake"
+  "/home/feng/xiaozhi/work/app4/build/font/CMakeFiles/font.dir/DependInfo.cmake"
+  "/home/feng/xiaozhi/work/app4/build/platform/src/CMakeFiles/lvgl_porting.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/app4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app4.dir/main.c.o"
+  "CMakeFiles/app4.dir/page_test.c.o"
+  "app4"
+  "app4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
